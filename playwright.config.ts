999dee2d8879e007144f5bc68f4b845513ca918a
Adela Šimnová <asimnova@freevision.sk>
2025-03-27 14:51:10 +0100

    Fix minor issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,13 +20,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173/",
+    baseURL: "http://localhost:5174/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:5173",
+    url: "http://localhost:5174",
     // reuseExistingServer: !process.env.CI,
   },
 });
